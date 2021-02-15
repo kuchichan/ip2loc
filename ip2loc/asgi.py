31bf1e33df35b218cf3c69cc3e53c7113f3a37d9
kuchichan <pawel.kucharski@hotmail.com>
2021-02-15 22:04:49 +0100

    Env variables support + blacked

--- ip2loc/asgi.py
+++ ip2loc/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ip2loc.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ip2loc.settings")
 
 application = get_asgi_application()
